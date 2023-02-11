195be8e3f0955b0e4bdd33294817f7cea1b34f0c
adel-macbook <69882714+adelina-lee@users.noreply.github.com>
2023-02-11 13:38:06 +0800

    Added Google Login button, not programmed

--- chattalk_frontend/src/components/Login.jsx
+++ chattalk_frontend/src/components/Login.jsx
@@ -10,7 +10,31 @@ const Login = () => {
   return (
     <div className="flex justify-start items-center flex-col h-screen">
         <div className="relative w-full h-full">
-          <video src={shareVideo}></video>
+          <video 
+            src={shareVideo}
+            type="video/mp4"
+            loop
+            controls={false}
+            muted
+            autoPlay
+            className="w-full h-full object-cover"
+          />
+        </div>
+
+        <div className="absolute flex flex-col justify-center">
+          <div className="p-5">
+            <img src={logo} alt="logo" width="130px" />
+          </div>
+          <div className="shadow-2xl">
+            <GoogleOAuthProvider>
+              <button
+                type="button"
+                className="bg-white flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                onclick="">
+                  <FcGoogle className="mr-4" /> Sign in with Google
+                </button>
+            </GoogleOAuthProvider>
+          </div>
         </div>
     </div>
   )
